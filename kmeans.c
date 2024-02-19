1af07cb535b6f2de7c25705c8b8dffe468ba6d25
Guy Cohen <guyco2204@gmail.com>
2024-02-19 20:54:39 +0200

    WIP: read file

--- kmeans.c
+++ kmeans.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int EPSILON = 0.001;
+#define EPSILON = 0.001;
 
 int main(int argc, char **argv)
 {
-
     int k;
+    int N;
+    int d;
     int iter;
     char *file_name;
     FILE *fptr;
-    int ch;
-    int tokenCount = 0;
-    double *point = NULL;
-    char *token = NULL;
-    double **data_points_arr = NULL;
-    int data_points_length = 0;
+    float data_points_arr[N][d];
 
-    if (argc > 4)
+    k = atoi(argv[1]);
+    N = atoi(argv[2]);
+    d = atoi(argv[3]);
+    iter = atoi(argv[4]);
+    file_name = argv[5];
+
+
+    if (k < 1 || k > N)
+    {
+        printf("Invalid number of clusters!");
+        return 1;
+    }
+
+    if (argc > 6)
     {
         return 1;
     }
 
-    if (argc == 4)
+    if (argc == 6)
     {
-        iter = atoi(argv[2]);
-        file_name = argv[3];
+        iter = atoi(argv[4]);
+        file_name = argv[5];
     }
-    else if (argc == 3)
+    else if (argc == 5)
     {
         iter = 200;
-        file_name = argv[2];
+        file_name = argv[4];
     }
     else
     {
@@ -43,66 +52,43 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    printf("Trying to open");
+
     fptr = fopen(file_name, "r");
 
-    while ((ch = fgetc(fptr)) != EOF)
+    printf("Opened");
+
+    float value;
+    for (int i = 0; i < N; i++)
     {
-        if (ch == ',' || ch == '\n')
+        for (int j = 0; j < d; j++)
         {
-            tokenCount++;
-            point = realloc(point, tokenCount * sizeof(double));
-            point[tokenCount - 1] = atof(token);
-            free(token);
-            token = NULL;
-
-            if (ch == '\n')
-            {
-                data_points_arr = realloc(data_points_arr, (data_points_length + 1) * sizeof(double *));
-                data_points_arr[data_points_length] = point;
-
-                tokenCount = 0;
-                point = NULL;
-                data_points_length++;
-            }
-        }
-        else
-        {
-            token = realloc(token, (tokenCount + 1) * sizeof(char));
-            token[tokenCount - 1] = ch;
-            tokenCount++;
+            scanf("%lf,", &value);
+            data_points_arr[i][j] = value;
         }
     }
-
-    printf("length --- %d", data_points_length);
 
     fclose(fptr);
 
-    k = atoi(argv[1]);
-    if (k < 1 || k > data_points_length)
+    for (int i = 0; i < N; i++)
     {
-        printf("Invalid number of clusters!");
-    }
-
-    printf("Data Points:\n");
-    for (int i = 0; i < data_points_length; i++)
-    {
-        printf("Point %d: (", i + 1);
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < d; j++)
         {
-            printf("%.2lf", data_points_arr[i][j]);
-            if (j < 3 - 1)
+            printf("%.4lf", data_points_arr[i][j]);
+            if (j < d - 1)
             {
                 printf(", ");
             }
         }
-        printf(")\n");
+        printf("\n");
     }
 
-    for (int i = 0; i < data_points_length; i++)
-    {
-        free(data_points_arr[i]);
-    }
-    free(data_points_arr);
+    // free data points allocation
+    // for (int i = 0; i < data_points_length; i++)
+    // {
+    //     free(data_points_arr[i]);
+    // }
+    // free(data_points_arr);
 
     return 0;
 }
